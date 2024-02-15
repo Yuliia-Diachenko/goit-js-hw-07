b590b18bf2550e7a3a8ad6f6be2dea84363d192c
Yuliia <123005661+Yuliia-Diachenko@users.noreply.github.com>
2024-02-15 14:18:25 +0200

    correct-style

--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,16 @@
+const button = document.querySelector("button");
+button.style = `border-radius: 8px;
+        border: none;
+        width: 86px;
+        height: 40px;
+        background-color: #4e75ff;
+        font-family: "Montserrat", sans-serif;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 1.5;
+        letter-spacing: 0.04em;
+        color: #fff;`
+
 const form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
